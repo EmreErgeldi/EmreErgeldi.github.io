220cc540f5c930d3e3155fd2cf4c94cf754581b5
FuzzyBang <85194698+FuzzyBang@users.noreply.github.com>
2022-07-27 19:56:17 +0300

    user URL bug fix

--- ui.js
+++ ui.js
@@ -10,7 +10,7 @@ class UI{
         <div class="row">
           <div class="col-md-3">
             <img class="img-fluid mb-2" src="${user.avatar_url}">
-            <a href="http://www.github.com/${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-1">View Profile</a>
+            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-1">View Profile</a>
           </div>
           <div class="col-md-9">
             <span class="badge bg-primary">Public Repos: ${user.public_repos}</span>
